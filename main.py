7003cb1213cf39f6eeeae61abfd3bf83bd39726e
Alexa <xla.2@yandex.ru>
2022-06-30 18:14:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import time
+
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+
+
+def l_analysis(af_path):
+    # moving import statements here results in ~1s gain!
+    import audioread.ffdec
+    import librosa
+    # ffmpeg binary backend must exist in the project folder if you want to read mp3 files with audioread
+    """takes a file by path returns mp3 audio file BPM"""
+    with audioread.audio_open(af_path) as m:
+        # print(f.channels, f.samplerate, f.duration)
+        lo = librosa.load(m)
+        # each librosa object is a tuple, (y=array, sr=sampling rate)
+        # lol.append(lo)
+        # beat_frames are subsections of y in the vicinity of each detected beat
+        tempo, beat_frames = librosa.beat.beat_track(y=lo[0], sr=lo[1])
+        return tempo, af_path
+
+# Standard loop takes ~9.5s
+# tict = time.perf_counter()
+# for p in fpl:
+#     tic = time.perf_counter()
+#     res = l_analysis(p)
+#     toc = time.perf_counter()
+#     print(f'Time spent: {toc - tic:.2f}', f'Tempo: {res:.2f}')
+
+# toct = time.perf_counter()
+# print(f'Time spent total: {toct-tict:.2f}')
+
+
+if __name__ == "__main__":
+    # NO PRINTING / ANY TIME-CONSUMING METHODS ABOVE THIS LINE!
+    FOLDER = 'test'
+    # build path from current folder
+    path = os.path.join("./", FOLDER)
+    # check that the directory exists
+    # print(path, os.path.isdir(path), os.getcwd())
+    # scan the directory and generate list of paths to the files we want
+    fpl = []
+    for e in os.listdir(path):
+        tp = os.path.join(path, e)
+        if os.path.isfile(tp) and e.lower().endswith('.mp3'):
+            fpl.append(tp)
+
+    # create librosa object list
+    # lol = []
+
+    # MP is a proper way to bypass GIL (1 io process lock w/ threading), now each process has its own GIL
+    # generate pool of processes, one per CPU core, almost no difference for core amount 4-16
+    pool = Pool(cpu_count())
+    tti = time.perf_counter()
+    with pool as pr:
+        # multiprocessor analogue of python's built-in map function
+        results = pool.map(l_analysis, fpl)
+    # I need context management here to ensure that all processes are killed after completion, otherwise:
+    # pool.close()  # 'TERM'
+    # pool.join()   # 'WAIT for processes to be killed'
+    tto = time.perf_counter()
+    print(f'Time spent total: {tto-tti:.2f}', results)
