9e56484d85a59cc42f915f824e68608a1734af0b
Alexa <xla.2@yandex.ru>
2022-07-06 20:04:25 +0300

    MT, MP and Single-threaded versions have been completed and compared.

--- main.py
+++ main.py
@@ -16,36 +16,40 @@ def l_analysis(af_path):
         tempo, beat_frames = librosa.beat.beat_track(y=lo[0], sr=lo[1])
         return tempo, af_path
 
-
+# STRANGE
 # import inspect
 # from collections.abc import Iterable
 # def ar_preloader(af_path):
-#     """creates a librosa object in memory"""
+#     """creates audioread object"""
 #     import audioread.ffdec
-#     # ffmpeg binary backend must exist in the project folder if you want to read mp3 files with audioread
+#     # !!! ffmpeg binary backend must exist in the project folder if you want to read mp3 files with audioread
 #     with audioread.audio_open(af_path) as m:
-#         print(m.channels, m.samplerate, m.duration)
-#         g = m
+#         # print(m.channels, m.samplerate, m.duration)
 #         # return m
-#     # l_analysis_o(g)
-#         print(type(g), inspect.isgenerator(g), isinstance(g, Iterable))
-#         ig = iter(g)
-#         # print(next(ig), 111)
-#     print(next(ig), 222)
-#     print(type(g), inspect.isgenerator(ig), isinstance(g, Iterable))
-#     # for frame in g:
+#         test = m
+#         # print(type(test), inspect.isgenerator(test), isinstance(test, Iterable))
+#         # print(next(iter(test)), 1111)
+#
+#     print(next(iter(test)), 2222)
+#     print(type(test), inspect.isgenerator(test), isinstance(test, Iterable))
+#
+#     # for frame in test:
 #     #     print(frame, 66)
+#
+#     # l_analysis_o(test)
+
+# ar_preloader('test/CASH GROWE - HARAKIRI.mp3')
 
 # def l_analysis_o(afo):
 #     """takes pre-loaded object from RAM?. Anyway, not by path. Returns mp3 audio file BPM"""
 #     import librosa
-#     import audioread.ffdec
-#     print(afo)
 #     lo = librosa.load(afo)
 #     # each librosa object is a tuple, (y=array, sr=sampling rate)
 #     # beat_frames are subsections of y in the vicinity of each detected beat, we just ignore those here
 #     tempo = librosa.beat.beat_track(y=lo[0], sr=lo[1])
 #     return tempo
+
+# l_analysis_o(ar_preloader('test/CASH GROWE - HARAKIRI.mp3'))
 
 
 if __name__ == "__main__":
@@ -62,32 +66,25 @@ if __name__ == "__main__":
         if os.path.isfile(tp) and e.lower().endswith('.mp3'):
             fpl.append(tp)
 
-    # lol = []
-    # tti = time.perf_counter()
-    # for p in fpl:
-    #     lol.append(ar_preloader(p))
-    # print(f'Time spent stage 1: {time.perf_counter()-tti:.2f}')
-    # ar_preloader(fpl[0])
-
     # SINGLE THREADED loop takes ~9.5s
-    # tict = time.perf_counter()
-    # for p in fpl:
-    #     tic = time.perf_counter()
-    #     res = l_analysis(p)
-    #     toc = time.perf_counter()
-    #     print(f'Time spent: {toc - tic:.2f}', f'Tempo: {res}')
-    # toct = time.perf_counter()
-    # print(f'Time spent total S: {toct-tict:.2f}')
+    tict = time.perf_counter()
+    for p in fpl:
+        tic = time.perf_counter()
+        res = l_analysis(p)
+        toc = time.perf_counter()
+        print(f'Time spent: {toc - tic:.2f}', f'Tempo: {res}')
+    toct = time.perf_counter()
+    print(f'Time spent total S: {toct-tict:.2f}')
 
-    # # MULTI-THREADED VERSION
-    # import concurrent.futures
-    # tti = time.perf_counter()
-    # with concurrent.futures.ThreadPoolExecutor() as execc:
-    #     results = execc.map(l_analysis, fpl)
-    # print(f'Time spent total MT: {time.perf_counter() - tti:.2f}')
-    # # for r in results:
-    # #     print(r)
-    # # MT version results in ~3.85s min!!!!
+    # MULTI-THREADED VERSION
+    import concurrent.futures
+    tti = time.perf_counter()
+    with concurrent.futures.ThreadPoolExecutor() as execc:
+        results = execc.map(l_analysis, fpl)
+    print(f'Time spent total MT: {time.perf_counter() - tti:.2f}')
+    # for r in results:
+    #     print(r)
+    # MT version results in ~3.85s min!!!!
 
     # MULTI-PROCESSED VERSION
     from multiprocessing import Pool, cpu_count
